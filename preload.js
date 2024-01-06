1066d46b872aff8c1a0c3f9af3332a2c14da5fd5
Umair Farooq <umair.farooq@osf-commerce.com>
2024-01-06 06:07:04 +0500

    Added example of DOMContentLoaded event

--- preload.js
+++ preload.js
@@ -1,9 +1,24 @@
-const { contextBridge, ipcRenderer } = require("electron");
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
 
-contextBridge.exposeInMainWorld("versions", {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke("ping"),
-  // we can also expose variables, not just functions
+    if (element) {
+      element.innerText = text;
+    }
+  };
+
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
 });
+
+// ipc tester code
+// const { contextBridge, ipcRenderer } = require("electron");
+
+// contextBridge.exposeInMainWorld("versions", {
+//   node: () => process.versions.node,
+//   chrome: () => process.versions.chrome,
+//   electron: () => process.versions.electron,
+//   ping: () => ipcRenderer.invoke("ping"),
+//   // we can also expose variables, not just functions
+// });
--- renderer.js
+++ renderer.js
@@ -2,10 +2,11 @@ const information = document.getElementById("info");
 
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
 
-const func = async () => {
-  const response = await window.versions.ping();
-  const ipcTester = document.getElementById("ipc-tester");
-  information.innerText = response;
-};
+// ipc tester code
+// const func = async () => {
+//   const response = await window.versions.ping();
+//   const ipcTester = document.getElementById("ipc-tester");
+//   information.innerText = response;
+// };
 
-func();
+// func();
