f7dbf9e0345ef374d1bb6aac2675abd9ec73ccc3
Umair Farooq <umair.farooq@osf-commerce.com>
2024-02-09 23:03:46 +0500

    Moved menu to ui layer from shared folder

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow, screen, Menu } = require("electron");
 const appResources = require("./resources/index").app;
-const menuTemplateBuilder = require("./shared/menu");
+const menuTemplateBuilder = require("./ui/menu");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
