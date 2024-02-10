6a49d8b6dca57703958cd391ef5515aa12e54d41
Umair Farooq <umair.farooq@osf-commerce.com>
2024-02-10 13:22:40 +0500

    Corrected AppAvatar test to use screen instead of render destructing

--- src/ui/components/AppAvatar/index.test.jsx
+++ src/ui/components/AppAvatar/index.test.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import AppAvatar from "./index";
 
 describe("When component AppAvatar is rendered", () => {
   test("it displays app name", () => {
-    const { getByRole } = render(<AppAvatar />);
+    render(<AppAvatar />);
 
-    const appNameHeading = getByRole("heading", { level: 1 });
+    const appNameHeading = screen.getByRole("heading", { level: 1 });
     expect(appNameHeading).toBeInTheDocument();
     expect(appNameHeading).toHaveTextContent(/^Business Books Keeper$/);
   });
